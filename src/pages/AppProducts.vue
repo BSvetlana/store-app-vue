407128234e0c90ef5b1d8539606ab3281ff1b64b
BSvetlana <svetlanabisacki@gmail.com>
2018-04-28 18:36:22 +0200

    product service

--- src/pages/AppProducts.vue
+++ src/pages/AppProducts.vue
@@ -1,17 +1,47 @@
 <template>
   <div>
-AppProducts
+    <div class="container mt-3 mr-1">
+        <div class="row">
+            <div class="col-6 mb-4">
+              <h4 style="color: #66BB6A;">Product list</h4>
+                <table class="table table-bordered">
+                    
+                    <thead style="background-color: #E8F5E9;">
+                        <tr>
+                            <th scope="col">Product</th>
+                            <th scope="col">Quantity</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(product,index) in products" :key="index">
+                            <td>{{ product.title }}</td>
+                            <td>{{ product.quantity }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { productService } from '../services/ProductService.js'
+
 export default {
   
+    data(){
+      return {
+        products: productService.list()
+      }
+    }
 }
 </script>
 
 <style scoped>
-
+.table>thead {
+    text-align: center
+}
 </style>
 
 
---
+++ src/services/ProductService.js
@@ -0,0 +1,17 @@
+
+const products = [
+    {id: 1, title: 'Computer', quantity: 3},
+    {id: 2, title: 'Keyboard', quantity: 4},
+    {id: 3, title: 'Monitor', quantity: 7},
+    {id: 4, title: 'Bag', quantity:13},
+    {id: 5, title: 'Cable', quantity: 5},
+]
+
+export default class ProductService {
+
+    list() {
+        return products;
+    }
+}
+
+export const productService = new ProductService();
